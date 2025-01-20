e74921fe19f13bb94d490bd5294f9d8c21a797fe
Milk4994 <111262763+Milk4994@users.noreply.github.com>
2025-01-20 09:58:24 +0900

    Update MakerNano_Mainkey.ino

--- src/MakerNano_Mainkey.ino
+++ src/MakerNano_Mainkey.ino
@@ -1,84 +1,49 @@
 //ジョイスティックの傾きに応じて文字入力をする
 #include <SPI.h>
 
-
-
 // MCP3208のCSピン設定
-
 const int CS_PIN_1 = 10; // MCP3208の1つ目のCSピン
-
 const int CS_PIN_2 = 9;  // MCP3208の2つ目のCSピン
-
 const int CS_PIN_3 = 8;  // MCP3208の3つ目のCSピン
 
-char Vowel_temp = "null";
 
+char Vowel_temp = "null";
 char Conso_temp = "null";
 
 
-
 const int A_PIN = 2;
-
 const int K_PIN = 3;
-
 const int S_PIN = 4;
-
 const int T_PIN = 5;
-
 const int N_PIN = 6;
-
 const int H_PIN = 7;
-
 const int M_PIN = A0;
-
 const int Y_PIN = A1;
-
 const int R_PIN = A2;
-
 const int W_PIN = A4;
-
 const int MK_PIN = A5;
 
-
-
+//傾きしきい値の設定
 const int LeftPush = 500;
-
 const int RightPush = 4000;
 
 
 
 void setup() {
-
   Serial.begin(9600); // シリアル通信の初期化
-
   SPI.begin();        // SPI通信の初期化
 
-
-
   // 各CSピンを出力モードに設定し、初期状態をHIGHにする
-
   pinMode(CS_PIN_1, OUTPUT);
-
   digitalWrite(CS_PIN_1, HIGH);
-
-
-
+  
   pinMode(CS_PIN_2, OUTPUT);
-
   digitalWrite(CS_PIN_2, HIGH);
-
-
-
+  
   pinMode(CS_PIN_3, OUTPUT);
-
   digitalWrite(CS_PIN_3, HIGH);
 
-
-
-  pinMode(CS_PIN_3, OUTPUT);
-
-
-
+  
   pinMode( A_PIN, INPUT_PULLUP);
   pinMode( K_PIN, INPUT_PULLUP);
   pinMode( S_PIN, INPUT_PULLUP);
@@ -91,360 +56,115 @@ void setup() {
   pinMode( W_PIN, INPUT_PULLUP);
   pinMode( MK_PIN, INPUT_PULLUP);
 
-  
-
 }
 
 
 
 void loop() {
-
   // MCP3208_1のデータを読み取る
-
   int value1_0 = readMCP3208(CS_PIN_1, 0); // MCP3208_3の0チャンネル
-
   int value1_1 = readMCP3208(CS_PIN_1, 1); // MCP3208_3の1チャンネル
-
   int value1_2 = readMCP3208(CS_PIN_1, 2); // MCP3208_3の2チャンネル
-
   int value1_3 = readMCP3208(CS_PIN_1, 3); // MCP3208_3の3チャンネル
-
   int value1_4 = readMCP3208(CS_PIN_1, 4); // MCP3208_3の0チャンネル
-
   int value1_5 = readMCP3208(CS_PIN_1, 5); // MCP3208_3の1チャンネル
-
   int value1_6 = readMCP3208(CS_PIN_1, 6); // MCP3208_3の2チャンネル
-
   int value1_7 = readMCP3208(CS_PIN_1, 7); // MCP3208_3の3チャンネル
 
-
-
   // MCP3208_2のデータを読み取る
-
   int value2_0 = readMCP3208(CS_PIN_2, 0); // MCP3208_3の0チャンネル
-
   int value2_1 = readMCP3208(CS_PIN_2, 1); // MCP3208_3の1チャンネル
-
   int value2_2 = readMCP3208(CS_PIN_2, 2); // MCP3208_3の2チャンネル
-
   int value2_3 = readMCP3208(CS_PIN_2, 3); // MCP3208_3の3チャンネル
-
   int value2_4 = readMCP3208(CS_PIN_2, 4); // MCP3208_3の0チャンネル
-
   int value2_5 = readMCP3208(CS_PIN_2, 5); // MCP3208_3の1チャンネル
-
   int value2_6 = readMCP3208(CS_PIN_2, 6); // MCP3208_3の2チャンネル
-
   int value2_7 = readMCP3208(CS_PIN_2, 7); // MCP3208_3の3チャンネル
 
-
-
   // MCP3208_3のデータを読み取る
-
   int value3_0 = readMCP3208(CS_PIN_3, 0); // MCP3208_3の0チャンネル
-
   int value3_1 = readMCP3208(CS_PIN_3, 1); // MCP3208_3の1チャンネル
-
   int value3_2 = readMCP3208(CS_PIN_3, 2); // MCP3208_3の2チャンネル
-
   int value3_3 = readMCP3208(CS_PIN_3, 3); // MCP3208_3の3チャンネル
-
   int value3_4 = readMCP3208(CS_PIN_3, 4); // MCP3208_3の0チャンネル
-
   int value3_5 = readMCP3208(CS_PIN_3, 5); // MCP3208_3の1チャンネル
-
   int value3_6 = readMCP3208(CS_PIN_3, 6); // MCP3208_3の2チャンネル
-
   int value3_7 = readMCP3208(CS_PIN_3, 7); // MCP3208_3の3チャンネル
 
-
-
-  // データをシリアルモニタに出力
-
-//   Serial.print("MCP3208_1 Channel 0: ");
-
-//   Serial.print(value1_0);
-
-//   Serial.print("\tChannel 1: ");
-
-//   Serial.println(value1_1);
-
-//   Serial.print("          Channel 2: ");
-
-//   Serial.print(value1_2);
-
-//   Serial.print("\tChannel 3: ");
-
-//   Serial.println(value1_3);
-
-//   Serial.print("          Channel 4: ");
-
-//   Serial.print(value1_4);
-
-//   Serial.print("\tChannel 5: ");
-
-//   Serial.println(value1_5);
-
-//   Serial.print("          Channel 6: ");
-
-//   Serial.print(value1_6);
-
-//   Serial.print("\tChannel 7: ");
-
-//   Serial.println(value1_7);
-
-
-
-//   Serial.print("MCP3208_2 Channel 0: ");
-
-//   Serial.print(value2_0);
-
-//   Serial.print("\tChannel 1: ");
-
-//   Serial.println(value2_1);
-
-//   Serial.print("          Channel 2: ");
-
-//   Serial.print(value2_2);
-
-//   Serial.print("\tChannel 3: ");
-
-//   Serial.println(value2_3);
-
-//   Serial.print("          Channel 4: ");
-
-//   Serial.print(value2_4);
-
-//   Serial.print("\tChannel 5: ");
-
-//   Serial.println(value2_5);
-
-//   Serial.print("          Channel 6: ");
-
-//   Serial.print(value2_6);
-
-//   Serial.print("\tChannel 7: ");
-
-//   Serial.println(value2_7);
-
-
-
-//   Serial.print("MCP3208_3 Channel 0: ");
-
-//   Serial.print(value3_0);
-
-//   Serial.print("\tChannel 1: ");
-
-//   Serial.println(value3_1);
-
-//   Serial.print("          Channel 2: ");
-
-//   Serial.print(value3_2);
-
-//   Serial.print("\tChannel 3: ");
-
-//   Serial.println(value3_3);
-
-//   Serial.print("          Channel 4: ");
-
-//   Serial.print(value3_4);
-
-//   Serial.print("\tChannel 5: ");
-
-//   Serial.println(value3_5);
-
-//   Serial.print("          Channel 6: ");
-
-//   Serial.print(value3_6);
-
-//   Serial.print("\tChannel 7: ");
-
-//   Serial.println(value3_7);
-
-
-
 //あ行
-
     if (digitalRead(A_PIN) == LOW)
-
     {
-
         Serial.println("a\t");
-
     }
-
-    if (value1_0 <= LeftPush)//横
+  
+    if (value1_0 <= LeftPush)
 
     {
-
         Serial.println("i\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "i";
-
     }
-
+  
     if (value1_0 >= RightPush)
-
     {
-
         Serial.println("e\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "e";
-
     }
-
-
 
     if (value1_1 <= LeftPush)
-
     {
-
         Serial.println("u\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "u";
-
     }
 
     if (value1_1 >= RightPush)
-
     {
-
         Serial.println("o\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "o";
-
     }
-
-
-
-    
-
 
 
 //か行
-
     if (digitalRead(K_PIN) == LOW)
-
     {
-
         Serial.println("ka\t");
-
     }
 
-    if (value1_2 <= LeftPush)//横
-
+    if (value1_2 <= LeftPush)
     {
-
-        Serial.println("ki\t");
-
-        Vowel_temp = "k";
-
-        Conso_temp = "i";
-
+      Serial.println("ki\t");
     }
 
     if (value1_2 >= RightPush)
-
     {
-
         Serial.println("ke\t");
-
-        Vowel_temp = "k";
-
-        Conso_temp = "e";
-
     }
 
-
-
     if (value1_3 <= LeftPush)
-
     {
-
         Serial.println("ku\t");
-
-        Vowel_temp = "k";
-
-        Conso_temp = "u";
-
     }
 
     if (value1_3 >= RightPush)
-
     {
-
         Serial.println("ko\t");
-
-        Vowel_temp = "k";
-
-        Conso_temp = "o";
-
     }
-
-    // if (digitalRead(K_PIN) == LOW)
-
-    // {
-
-    //     Serial.print("ka\t");
-
-    // }
-
-    
+   
 
 //さ行
-
     if (digitalRead(S_PIN) == LOW)
-
     {
-
         Serial.println("sa\t");
-
     }
 
-    if (value1_4 <= LeftPush)//横
-
+    if (value1_4 <= LeftPush)
     {
-
         Serial.println("si\t");
-
-        Vowel_temp = "s";
-
-        Conso_temp = "i";
-
     }
 
     if (value1_4 >= RightPush)
-
     {
-
         Serial.println("se\t");
-
-        Vowel_temp = "s";
-
-        Conso_temp = "e";
-
     }
 
-
-
     if (value1_5 <= LeftPush)
-
     {
-
         Serial.println("su\t");
-
-        Vowel_temp = "s";
-
-        Conso_temp = "u";
-
     }
 
     if (value1_5 >= RightPush)
@@ -929,7 +649,7 @@ void loop() {
 
     }
 
-    if (value3_6 <= LeftPush)//横
+    if (value3_6 <= LeftPush)
 
     {
 
@@ -942,100 +662,40 @@ void loop() {
     }
 
     if (value3_6>= RightPush)
-
     {
-
         Serial.println("!\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "!";
-
     }
-
-
-
     if (value3_7 <= LeftPush)
-
     {
-
         Serial.println("?\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "?";
-
     }
-
     if (value3_7 >= RightPush)
-
     {
-
         Serial.println("…\t");
-
-        Vowel_temp = "null";
-
-        Conso_temp = "_";
-
-    }
-
-    // if (digitalRead(H_PIN) == LOW)
-
-    // {
-
-    //     Serial.print("ha\t");
-
-    // }
-
-
-
-    
+    }   
 
 
 
   delay(300); // 待機
-
 }
-
 
 
 // MCP3208からデータを読み取る関数
-
 int readMCP3208(int csPin, int channel) {
-
   if (channel < 0 || channel > 7) {
-
     return -1; // チャンネルが無効な場合
-
   }
-
-
 
   digitalWrite(csPin, LOW); // 指定されたCSピンをLOWに設定して通信開始
 
-
-
   // MCP3208へのコマンド送信
-
   byte command = 0b00000110 | ((channel & 0x04) >> 2); // スタートビットとシングルエンド設定
-
   SPI.transfer(command);
-
   byte result1 = SPI.transfer((channel & 0x03) << 6);  // チャンネルビット送信
-
   byte result2 = SPI.transfer(0x00);                  // データの受信
-
-
-
   digitalWrite(csPin, HIGH); // 指定されたCSピンをHIGHに戻して通信終了
 
-
-
   // 12ビットの結果を計算
-
   int value = ((result1 & 0x0F) << 8) | result2;
-
   return value;
-
 }
-
